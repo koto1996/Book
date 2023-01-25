202bd69206ac3af869ec20dc76acca58e4fe0db1
kotoe <121812120+koto1996@users.noreply.github.com>
2023-01-25 18:04:18 +0000

    アプリケーション２課題add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
    def create
     @book = Book.new(book_params)
     if@book.save
+    flash[:notice]="successfully"
     redirect_to book_path(@book.id)
     else
     render :index
@@ -26,6 +27,7 @@ class BooksController < ApplicationController
   def update
     @book=Book.find(params[:id])
     if@book.update(book_params)
+    flash[:notice]="successfully"
     redirect_to book_path(@book.id)
     else
     render :edit
@@ -35,6 +37,7 @@ class BooksController < ApplicationController
   def destroy
     book=Book.find(params[:id])
     book.destroy
+    flash[:notice]="successfully"
     redirect_to'/books'
   end
 
